0356b9fc841f2cd1f6418e93e615f2e60fdd52f6
kim <31045060+kitorer@users.noreply.github.com>
2020-04-20 22:15:25 -0400

    SENDS photos

--- main.py
+++ main.py
@@ -2,19 +2,44 @@ import logging
 import telegram
 from telegram.ext import Updater
 from telegram.ext import CommandHandler
+import random 
+import datetime 
+from datetime import timedelta
 
-# Enable logging
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                    level=logging.INFO)
 
-logger = logging.getLogger(__name__)
-updater = Updater(token='token', use_context=True)
+from telegram.ext import Updater
+updater = Updater(token='976356887:AAEmSQSSWc97LBSMZsPSytgIr3ExJuQWe4g', use_context=True)
 dispatcher = updater.dispatcher
-token = '976356887:AAEmSQSSWc97LBSMZsPSytgIr3ExJuQWe4g' #Token of your bot
-start_handler = CommandHandler('start', start)
-dispatcher.add_handler(start_handler)
-updater.start_polling()
+import logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                     level=logging.INFO)
 
 def start(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
+    context.bot.send_message(chat_id=update.effective_chat.id, text="i only know how to echo dur !")
 
+from telegram.ext import CommandHandler
+start_handler = CommandHandler('start', start)
+dispatcher.add_handler(start_handler)
+
+updater.start_polling()
+#######################################################################################################
+def echo(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
+
+from telegram.ext import MessageHandler, Filters
+echo_handler = MessageHandler(Filters.text & (~Filters.command), echo)
+dispatcher.add_handler(echo_handler)
+
+def caps(update, context):
+    text_caps = ' '.join(context.args).upper()
+    context.bot.send_message(chat_id=update.effective_chat.id, text=text_caps)
+
+caps_handler = CommandHandler('caps', caps)
+dispatcher.add_handler(caps_handler)
+
+
+def nendsudes(update,context):
+     if(timedelta.hour==0):
+        context.bot.send_photo(chat_id=update.effective_chat.id,photo ='https://i.imgur.com/7G4KARD.jpg',caption='they turned me into a black pickle')
+
+print('starting...')
